5025383169c5cb6b3d01d327a92a3054dd9641c1
Jeremy Cromwell <jeremy.cromwell@gmail.com>
2021-01-28 15:10:38 -0800

    revert

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Jeremy's Sandbox</title>
+        <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
